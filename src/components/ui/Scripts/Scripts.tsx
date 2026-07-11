6b521f9d50694215cbaa085340d923ddf59eff18
Artem Zhyto <artemzhr11@gmail.com>
2026-07-11 14:23:38 +0300

    the footer is set to automatically insert the year

--- src/components/ui/Scripts/Scripts.tsx
+++ src/components/ui/Scripts/Scripts.tsx
@@ -5,14 +5,16 @@ import './Scripts.scss'
 import { scripts } from '@config/scripts'
 
 const Scripts = () => {
+  const currentYear = new Date().getFullYear()
+
   return (
-    <div className='Scripts bg-(--bl) border-r border-(--border) min-w-27.5 w-[29.25vw] h-screen flex flex-col justify-start items-center overflow-y-auto overflow-x-hidden'>
+    <div className='Scripts bg-(--bl) border-r border-(--border) min-w-27.5 max-w-225 w-[29.25vw] h-screen flex flex-col justify-start items-center overflow-y-auto overflow-x-hidden'>
       <div className='Scripts_content flex flex-col justify-start items-center gap-12.5 py-12.5'>
         {scripts.map((script) => (
           <button
             key={script.title}
             title={script.descr}
-            className='Scripts_btn max-w-90 mx-12.5 px-6.25 py-5 self-stretch text-[clamp(0.625rem,1.5vw,1.25rem)] rounded-[20px] shrink-0 text-center cursor-pointer'
+            className='Scripts_btn max-w-90 mx-12.5 px-6.25 py-5 self-stretch text-[clamp(0.5rem,1.5vw,1.125rem)] rounded-[20px] shrink-0 text-center cursor-pointer'
           >
             {script.title}
           </button>
@@ -20,7 +22,7 @@ const Scripts = () => {
       </div>
 
       <footer className='Scripts_footer mt-auto p-12.5 w-full min-w-27.5 max-w-[29.25vw] border-t border-(--border) bg-(--bl) shrink-0 flex justify-center items-center text-center text-[clamp(0.5rem,1.5vw,1rem)] text-(--o-txt)'>
-        © 2026 QuCore Dynamic. All rights reserved
+        © {currentYear} QuCore Dynamic. All rights reserved
       </footer>
     </div>
   )
